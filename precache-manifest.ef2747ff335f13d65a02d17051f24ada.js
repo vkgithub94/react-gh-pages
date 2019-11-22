7cb856337c526ac49defb0f79ef5491a702b4cb8
Vikash <vikashp@orowealth.com>
2019-11-22 19:52:39 +0530

    Updates

---
+++ precache-manifest.ef2747ff335f13d65a02d17051f24ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bf4a66fa3f158511bc72efba245556",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "122e86ccb66331287868",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/react-gh-pages/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "122e86ccb66331287868",
+    "url": "/react-gh-pages/static/js/main.d1e12586.chunk.js"
+  },
+  {
+    "revision": "3703c47d598af9a7e5b4",
+    "url": "/react-gh-pages/static/js/runtime-main.ead2228f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
